4d24da32cbfa539a9a547755349522f245711fb3
ZachElkins <zach.elkins@gmil.com>
2021-12-15 17:20:15 -0700

    finished stack class

--- src/data_structures/stack.py
+++ src/data_structures/stack.py
@@ -1,14 +1,55 @@
 class Stack:
+    """
+    A class for a first-in-last-out data structure.
+    ...
+    Methods
+    _______
+    push(item):
+        Adds the item to the top of the stack.
+    pop():
+        Removes and returns the top item the stack.
+    peek():
+        Returns the item on top of the stack.
+    empty():
+        Returns True if the stack is empty, otherwise False.
+    """
+
     def __init__(self):
+        """ Initializes the stack to an empty list."""
         self.stack = []
 
     def push(self, item):
+        """ Adds a new item to the top of the stack.
+        :param item: The new item to add to the stack.
+        :return:
+        """
         self.stack = [item] + self.stack
 
     def pop(self):
+        """
+        Removes and returns the top item the stack.
+        :return:
+        """
+        if self.empty():
+            raise LookupError("The stack is empty.")
         item = self.stack[0]
         self.stack = self.stack[1:]
         return item
 
     def peek(self):
+        """
+        peek():
+            Returns the item on top of the stack.
+        :return:
+        """
+        if self.empty():
+            raise LookupError("The stack is empty.")
         return self.stack[0]
+
+    def empty(self):
+        """
+        empty():
+            Returns True if the stack is empty, otherwise False.
+        :return:
+        """
+        return len(self.stack) == 0
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,6 +1,45 @@
+import pytest
 from data_structures.stack import Stack
 
 
-def test_init():
+def test_stack_init():
     stack = Stack()
     assert type(stack) == Stack
+
+
+def test_stack_empty():
+    stack = Stack()
+    assert stack.empty() is True
+
+
+def test_stack_push():
+    stack = Stack()
+    stack.push(1)
+    assert stack.empty() is False
+    assert stack.pop() == 1
+
+
+def test_stack_peek():
+    stack = Stack()
+    stack.push(1)
+    assert stack.peek() == 1
+    assert stack.empty() is False
+
+
+def test_stack_peek_lookup_error():
+    stack = Stack()
+    with pytest.raises(LookupError):
+        stack.peek()
+
+
+def test_stack_pop():
+    stack = Stack()
+    stack.push(1)
+    assert stack.pop() == 1
+    assert stack.empty() is True
+
+
+def test_stack_pop_lookup_error():
+    stack = Stack()
+    with pytest.raises(LookupError):
+        _ = stack.pop()
